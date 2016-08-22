505198e2249b727b0bd5610aa65723449e0d458c
ummis <swathi.ummineni@campsystems.com>
2016-08-22 16:10:42 +0530

    first commit

---
+++ Challenge2Test/Challenge2Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Challenge2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge2Test
+{
+    [TestClass]
+    public class Challenge2Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var challenge2 = new Encryption();
+            challenge2.Encrypt("have a nice day");
+        }
+
+        [TestMethod]
+        public void DecryptMessage()
+        {
+            var decryption = new Decryption();
+
+            decryption.Decrypt("hae and via ecy numsp 5 6 10");
+
+        }
+    }
+}
